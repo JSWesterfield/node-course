6c32ee908a0a7b85ccd9ca5f76f460a9fb610e32
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-02-07 12:57:33 -0500

    Add in process.argv for seeing all of the arguments that node provides

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ console.log(msg)
 
 const greenMsg = chalk.blue.inverse.bold('success')
 console.log(greenMsg)
+
+console.log(process.argv) //in NodeJS == 'Arguement v' an array that contains all the arguments provided
