3be282a1482eca44c049c0439b388d8f11f46a61
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-02-07 12:07:59 -0500

    first commit for node-course

---
+++ notes.js
@@ -0,0 +1,7 @@
+const getNotes = function () {
+    return 'Your notes..';
+}
+
+module.exports = getNotes;  // Exports Literal
+//export default getNotes;  // Exports Literal converted to ES6 Module
+// exports.getNotes OR module.exports.getNotes, we add a property to the exports object
---
+++ utils.js
@@ -0,0 +1,7 @@
+// File which contains a few useful utilities for our application
+console.log('utils.js')
+
+const name = 'Mike'
+const add = function (a, b) {
+    return a + b
+}
