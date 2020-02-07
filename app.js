93261803c931cb4e07fbd7aacfbac40dc25e069a
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-02-07 14:09:30 -0500

    adds Google Cloud Storage for uploading Project and deploying in the Google Cloud

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
-
-const greenMsg = chalk.blue.inverse.bold('success')
-console.log(greenMsg)
-
-//console.log(process.argv) //in NodeJS == 'Arguement v' an array that contains all the arguments provide-should show an 'array of all my values' from my console.logs
-console.log(process.argv[2]) // retrieve 3rd index value from our arguments array
