f2651bcf194a93936f866ef9d3338c9a1f141ebc
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-02-08 02:55:29 -0500

    adds in yargs add command

--- app.js
+++ app.js
@@ -5,4 +5,15 @@ const getNotes = require('./notes.js')
 // Customize yargs version
 yargs.version('1.1.0')
 
+// add, remove, read, list
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('adding a new note!')
+    }
+})
+
 console.log(yargs.argv)
