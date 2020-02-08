f22f586e69dfdf9199d0ca28bca8252d96773fa1
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-02-08 01:56:52 -0500

    add yargs versioning tool

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const command = process.argv[2]
+// Customize yargs version
+yargs.version('1.1.0')
 
-console.log(process.argv)
-
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
-
-//next add yargs
+console.log(yargs.argv)
