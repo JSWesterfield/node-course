34309bc7757c164daf59546c878088b4052b6917
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-02-24 12:47:05 -0500

    removes parse function and adds a fileWriteSync to dynamically add files

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,11 +1,9 @@
+const fs = require('fs');
+
 const book = {
     title: "Ego is the Enemy",
     author: "Ryan Holiday"
 }
 
 const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
-
-// Opposite of JSON.stringify
-const parsedData = JSON.parse(bookJSON); //returns the parsed JSON
-console.log(parsedData.author); //Ryan holiday
+fs.writeFileSync('1-json.json', bookJSONnode);
