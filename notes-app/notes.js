78fc8b9ebc3ce1b188d563dd5c59602137313657
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-02-25 04:37:30 -0500

    adds in loadNotes function and adds dataBuffer var set to fs.readFileSync and dataJSON var set to the dstringified dataBuffer or the bit data, lastly we added a return statement that sets the dataJSON variable into a JSON.parse method

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,10 +1,16 @@
-//GET
+const fs = require('fs')
 const getNotes = function () {
-    return 'Your notes..';
+    return 'Your notes..'
 }
 
 const addNote = function (title, body) {
+    const notes = loadNotes()
+}
 
+const loadNotes = function() {
+    const dataBuffer = fs.readFileSync('notes.json')
+    const dataJSON = dataBuffer.toString()
+    return JSON.parse(dataJSON)
 }
 
 //module.exports = getNotes //how to store more than one object?
