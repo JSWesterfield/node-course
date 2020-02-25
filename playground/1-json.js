f61c7f6690cf0ca5158092a7b8fdfcad3649682c
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-02-24 22:08:07 -0500

    dataBuffer data returned as a string with the toString method

--- playground/1-json.js
+++ playground/1-json.js
@@ -8,4 +8,5 @@ const fs = require('fs');
 // fs.writeFileSync('1-json.json', bookJSON);
 
 const dataBuffer = fs.readFileSync('1-json.json');  //returns json formatted data from the 1-json.json file
-console.log(dataBuffer); //a string isnt returned, but a buffer, which nodejs to represent data binary data.
+//console.log(dataBuffer); //a string isnt returned, but a buffer, which nodejs to represent data binary data.
+console.log(dataBuffer.toString()); //to get data back we use a toString method 
