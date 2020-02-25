76ecb2517590c58b0914b0fe1ddd574246a3925b
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-02-24 22:15:05 -0500

    adds console.log data.title

--- playground/1-json.js
+++ playground/1-json.js
@@ -8,5 +8,8 @@ const fs = require('fs');
 // fs.writeFileSync('1-json.json', bookJSON);
 
 const dataBuffer = fs.readFileSync('1-json.json');  //returns json formatted data from the 1-json.json file
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data.title); 
 //console.log(dataBuffer); //a string isnt returned, but a buffer, which nodejs to represent data binary data.
-console.log(dataBuffer.toString()); //to get data back we use a toString method 
+//console.log(dataBuffer.toString()); //to get data back we use a toString method 
