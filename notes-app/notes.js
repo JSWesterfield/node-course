2acdfb7191e2740eb6cd24301f97c1684df0c488
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-02-25 03:56:19 -0500

    addNote function created, module.exports object refactored to export more than one function to app.js

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,7 +1,14 @@
+//GET
 const getNotes = function () {
     return 'Your notes..';
 }
 
-module.exports = getNotes;  // Exports Literal
-//export default getNotes;  // Exports Literal converted to ES6 Module
-// exports.getNotes OR module.exports.getNotes, we add a property to the exports object
+const addNote = function (title, body) {
+
+}
+
+//module.exports = getNotes //how to store more than one object?
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
