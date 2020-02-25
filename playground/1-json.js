ce599e9e4b98f0dd829a8ed5b7cff98e5d9eec89
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-02-24 23:57:57 -0500

    adds challenge and how to implement

--- playground/1-json.js
+++ playground/1-json.js
@@ -7,9 +7,17 @@ const fs = require('fs');
 // const bookJSON = JSON.stringify(book);
 // fs.writeFileSync('1-json.json', bookJSON);
 
-const dataBuffer = fs.readFileSync('1-json.json');  //returns json formatted data from the 1-json.json file
-const dataJSON = dataBuffer.toString();
-const data = JSON.parse(dataJSON);
-console.log(data.title); 
+// const dataBuffer = fs.readFileSync('1-json.json');  //returns json formatted data from the 1-json.json file
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// console.log(data.title); 
 //console.log(dataBuffer); //a string isnt returned, but a buffer, which nodejs to represent data binary data.
 //console.log(dataBuffer.toString()); //to get data back we use a toString method 
+
+//
+// Challenge: work with JSON and the file system
+//
+// 1. Load and parse the JSON data
+// 2. Change the name and age property using your info
+// 3. Stringify the changed object and overwrite the original data 
+// 4. Test your work by viewing data in the JSON file
