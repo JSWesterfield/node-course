eccecca4d5c5d6ff5bce93ab0d1f7cce504718b1
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-03-02 23:02:40 -0500

    reverted back to previous working version

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,21 +5,19 @@ const notes = require('./notes.js')
 // Customize yargs version
 yargs.version('1.1.0')
 
-// add, remove, read, list
-
 // Create add command
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    builder: { 
+    builder: {
         title: {
             describe: 'Note title',
-            demandOption: true, 
-            type: 'string'  
+            demandOption: true,
+            type: 'string'
         },
         body: {
             describe: 'Note body',
-            demandOption: true, 
+            demandOption: true,
             type: 'string'
         }
     },
@@ -28,7 +26,7 @@ yargs.command({
     }
 })
 
-// Create remove command git add
+// Create remove command
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
@@ -37,23 +35,22 @@ yargs.command({
     }
 })
 
-// Create a list command
+// Create list command
 yargs.command({
     command: 'list',
-    describe: 'Listing out all notes',
+    describe: 'List your notes',
     handler: function () {
-        console.log('Lists notes')
+        console.log('Listing out all notes')
     }
 })
 
-// Create a read command
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
 
-//
-// Challenge: Add two new commands
-//
-// 1. Setup command to support "list" command (print placeholder message for now)
-// 2. Setup command to support "read" command (pring placeholder message for now)
-// 3. Test your work both commands and ensure correct output
-
-yargs.parse() // yargs parses with configuration details
-//console.log(yargs.argv) //console.logs our yargs object and the argv property
+yargs.parse()
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,23 +1,16 @@
 const fs = require('fs')
 
-// Order: we want to:
-// 1.) Save data 
-// 2.) Take the object or an array
-// 3.) We use JSON stringify and then
-// 4.) We write it to the fiesystem (fs)
-
 const getNotes = function () {
-    return 'Your notes..'
+    return 'Your notes...'
 }
 
 const addNote = function (title, body) {
     const notes = loadNotes()
     const duplicateNotes = notes.filter(function (note) {
         return note.title === title
-    })// verify notes exist - w/ array filter method, loop through array to find duplicate values for note.title property 
+    })
 
-    if (duplicateNotes.length === 0) { // duplicateNotes array is set to length of 0 nothing was passed into the array, so its empty, and no duplicates exist
-        // no duplicates exist? ....push new note into notes.json
+    if (duplicateNotes.length === 0) {
         notes.push({
             title: title,
             body: body
@@ -27,26 +20,23 @@ const addNote = function (title, body) {
     } else {
         console.log('Note title taken!')
     }
-
 }
 
-const saveNotes = function (notes) {  //takes 
-    const dataJSON = JSON.stringify(notes) //store stringified JSON notes data to dataJSON const
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function() {
+const loadNotes = function () {
     try {
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString()
         return JSON.parse(dataJSON)
-    } catch(e) {
-        return [] //add an empty array if nothing exists
+    } catch (e) {
+        return []
     }
-    
 }
 
-//module.exports = getNotes //how to store more than one object?
 module.exports = {
     getNotes: getNotes,
     addNote: addNote
